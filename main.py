66f7ced2e12ee0b734b5884776a4b8fc463caa0b
22046-NelsonEscalante <esc22046@uvg.edu.gt>
2023-02-26 15:59:58 -0600

    Added the main. Can create RAM and Processes

---
+++ main.py
@@ -0,0 +1,17 @@
+from simpy import *
+import random
+
+def makeRAM(env, init, cap):
+    RAM = Container(env, init=init, capacity=cap)
+    return RAM
+
+def newProcess():
+    memorySize = random.randint(1, 10)
+    instructions = random.randint(1, 10)
+    att = [memorySize, instructions]
+    return att
+
+env = Environment()
+
+Memory = makeRAM(env, 100, 100)
+
